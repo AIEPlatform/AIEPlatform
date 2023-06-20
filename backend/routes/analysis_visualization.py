bc516e242b96047a40b3eab45abe2032c4b4a126
ChenPanXYZ <me@chenpan.xyz>
2023-06-20 14:09:26 -0400

    reoranized

---
+++ backend/routes/analysis_visualization.py
@@ -0,0 +1,153 @@
+from credentials import *
+from flask import Blueprint, session
+from flask import request
+from bson import json_util
+from bson.objectid import ObjectId
+from helpers import *
+from Models.DeploymentModel import DeploymentModel
+from Models.DatasetModel import DatasetModel
+from errors import *
+import pandas as pd
+import pickle
+from bson.objectid import ObjectId
+from flask import send_file, make_response
+from Analysis.basic_reward_summary_table import basic_reward_summary_table
+from Analysis.AverageRewardByTime import AverageRewardByTime
+
+analysis_visualization_apis = Blueprint('analysis_visualization_apis', __name__)
+
+@analysis_visualization_apis.route("/apis/upload_local_modification", methods = ["PUT"])
+def upload_local_modification():
+
+    csv_file = request.files['csvFile']
+    df = pd.read_csv(csv_file)
+
+    print(df['outcome.timestamp'])
+
+    datasetId = request.form['datasetId'] if 'datasetId' in request.form else None
+
+    response = DatasetModel.update_one(datasetId, df)
+
+    if response == 200:
+        return status_code("UPLOAD_LOCAL_MODIFICATION_SUCCESSFUL")
+    else:
+        # TODO
+        return status_code("DEFAULT_SERVER_ERROR")
+
+
+
+
+
+@analysis_visualization_apis.route("/apis/analysis/downloadArrowDataset/<id>", methods=["GET"])
+def downloadArrowDataset(id):
+    if check_if_loggedin() is False:
+        return json_util.dumps({
+            "status_code": 403,
+        }), 403 
+    
+    try:
+        dataset = DatasetModel.get_one({"_id": ObjectId(id)})
+
+        df = pickle.loads(dataset['dataset'])
+        csv_string = df.to_csv(index=False)
+
+        # Create a Flask response object with the CSV data
+        response = make_response(csv_string)
+
+        # Set the headers to tell the browser to download the file as a CSV
+        response.headers['Content-Disposition'] = f'attachment; filename={dataset["name"]}_{dataset["createdAt"]}.csv'
+        response.headers['Content-type'] = 'text/csv'
+        return response
+    except:
+        return json_util.dumps({
+            "status_code": 500, 
+            "message": "downloading error"
+        }), 500
+
+
+
+
+
+
+@analysis_visualization_apis.route("/apis/analysis/deleteArrowDataset/<id>", methods=["DELETE"])
+def delete_arrow_dataset(id):
+    if check_if_loggedin() is False:
+        return json_util.dumps({
+            "status": 403,
+        }), 403 
+    
+    try:
+        dataset = DatasetModel.delete(ObjectId(id))
+        return json_util.dumps({
+            "status": 200,
+            "message": "Dataset is deleted."
+        }), 200
+    except:
+        return json_util.dumps({
+            "status": 500, 
+            "message": "deleting error"
+        }), 500
+
+
+
+
+
+@analysis_visualization_apis.route("/apis/analysis/get_deployment_datasets", methods=["GET"])
+def get_deployment_datasets():
+    # load from params.
+    deployment = request.args.get('deployment')
+    the_deployment = DeploymentModel.get_one({"name": deployment})
+    the_datasets = DatasetModel.get_many({"deploymentId": the_deployment['_id']}, {'dataset': 0})
+
+    return json_util.dumps(
+        {
+        "status_code": 200,
+        "datasets": the_datasets
+        }
+    ), 200
+
+
+@analysis_visualization_apis.route("/apis/analysis/basic_reward_summary_table", methods = ["POST"])
+def basic_reward_summary_table_api():
+    theDatasetId = request.json['theDatasetId'] if 'theDatasetId' in request.json else None # This is the id.
+    
+    selected_variables = request.json['selectedVariables'] if 'selectedVariables' in request.json else None
+ 
+    if theDatasetId is None or selected_variables is None:
+        return json_util.dumps({
+            "status_code": 400,
+            "message": "Please make sure the_study_basic_info, selected_variables are provided."
+        }), 400
+    else:
+        df = getDataset(theDatasetId)
+        result_df = basic_reward_summary_table(df, selected_variables)
+        return json_util.dumps({
+            "status_code": 200,
+            "message": "Table returned.",
+            "result": {
+                "columns": list(result_df.columns), 
+                "rows": [tuple(r) for r in result_df.to_numpy()]
+            }
+        }), 200
+
+
+@analysis_visualization_apis.route("/apis/analysis/AverageRewardByTime", methods = ["POST"])
+def AverageRewardByTime_api():
+    theDatasetId = request.json['theDatasetId'] if 'theDatasetId' in request.json else None # This is the id.
+    
+ 
+    if theDatasetId is None:
+        return json_util.dumps({
+            "status_code": 400,
+            "message": "Please make sure the_study_basic_info, selected_variables are provided."
+        }), 400
+    else:
+        df = getDataset(theDatasetId)
+        result_df, groups = AverageRewardByTime(df, [])
+        return json_util.dumps({
+            "status_code": 200,
+            "message": "Table returned.",
+            "data": result_df, 
+            "groups": groups
+        }), 200
+    
---
+++ backend/routes/user_interaction.py
@@ -0,0 +1,252 @@
+from credentials import *
+from flask import Blueprint, session
+from flask import request
+from bson import json_util
+from bson.objectid import ObjectId
+from helpers import *
+from Policies.UniformRandom import UniformRandom
+from Policies.ThompsonSamplingContextual import ThompsonSamplingContextual
+import datetime
+import time
+import threading
+from Models.VariableValueModel import VariableValueModel
+from Models.InteractionModel import InteractionModel
+from Models.MOOCletModel import MOOCletModel
+from Models.StudyModel import StudyModel
+from Models.DeploymentModel import DeploymentModel
+from Models.VariableModel import VariableModel
+from errors import *
+import traceback
+from bson.objectid import ObjectId
+
+user_interaction_apis = Blueprint('user_interaction_apis', __name__)
+
+def create_mooclet_instance(the_mooclet):
+    cls = globals().get(the_mooclet['policy'])
+    if cls:
+        return cls(**the_mooclet)
+    else:
+        raise ValueError(f"Invalid policy name")
+
+def get_mooclet_for_user(deployment_name, study_name, user):
+    # Note that, a deployment + study_name + user uniquely identify a mooclet.
+    # OR, this function will decide one and return.
+    deployment = DeploymentModel.get_one({'name': deployment_name}, public = True)
+    study = StudyModel.get_one({'deploymentId': ObjectId(deployment['_id']), 'name': study_name})
+    # TODO: Check if re-assignment is needed. For example, the mooclet is deleted (not yet implemented), or the experiment requires re-assignment at a certain time point.
+
+
+    # Find the latest interaction.
+    the_interaction = InteractionModel.find_last_interaction(study, user)
+
+    if the_interaction is not None:
+        the_mooclet = MOOCletModel.find_mooclet({'_id': the_interaction['moocletId']})
+        return (the_mooclet)
+    else:
+        root_mooclet = MOOCletModel.find_mooclet({"_id": study['rootMOOClet']})
+        return inductive_get_mooclet(root_mooclet, user)
+
+def assign_treatment(deployment_name, study_name, user, where = None, other_information = None):
+    start_time = time.time()
+    the_mooclet = get_mooclet_for_user(deployment_name, study_name, user)
+    try:
+        mooclet = create_mooclet_instance(the_mooclet)
+        version_to_show = mooclet.choose_arm(user, where, other_information)
+
+        print(version_to_show)
+        if DEV_MODE:
+            end_time = time.time()
+            execution_time = end_time - start_time
+
+            the_log = {
+                "policy": the_mooclet['policy'],
+                "execution_time": execution_time, 
+                "threads": threading.active_count(), 
+                "timestamp": datetime.datetime.now()
+            }
+            TreatmentLog.insert_one(the_log)
+        return version_to_show
+    except Exception as e:
+        if DEV_MODE:
+            the_log = {
+                "policy": the_mooclet['policy'],
+                "error": True,
+                "error_message": str(e),
+                "traceback": traceback.format_exc(),
+                "threads": threading.active_count(), 
+                "timestamp": datetime.datetime.now()
+            }
+            TreatmentLog.insert_one(the_log)
+        return None
+
+def get_reward(deployment_name, study_name, user, value, where = None, other_information = None):
+    # Get MOOClet!
+    try:
+        start_time = time.time()
+        the_mooclet = get_mooclet_for_user(deployment_name, study_name, user)
+        mooclet = create_mooclet_instance(the_mooclet)
+        response = mooclet.get_reward(user, value, where, other_information)
+
+        if DEV_MODE:
+            end_time = time.time()
+            execution_time = end_time - start_time
+
+            the_log = {
+                "policy": the_mooclet['policy'],
+                "execution_time": execution_time, 
+                "threads": threading.active_count(), 
+                "timestamp": datetime.datetime.now()
+            }
+            RewardLog.insert_one(the_log)
+        return response
+    except Exception as e:
+        if DEV_MODE:
+            the_log = {
+                "policy": the_mooclet['policy'],
+                "error": True, 
+                "threads": threading.active_count(), 
+                "timestamp": datetime.datetime.now()
+            }
+            RewardLog.insert_one(the_log)
+
+
+
+# https://security.stackexchange.com/questions/154462/why-cant-we-use-post-method-for-all-requests
+# GET : does not change anything server side, multiple GET with same parameters should get same response - typically get an account value
+# POST : can make changes server side, multiple POST with same parameters can lead to different results and responses - typically add an amount to an account
+# PUT : can make changes server side, multiple PUT with same parameters should lead to same result and response - typically set an account value
+
+@user_interaction_apis.route("/apis/get_treatment", methods=["POST"])
+def get_treatment():
+    # read request body.
+    try:
+        start_time = time.time()
+        deployment = request.json['deployment'] if 'deployment' in request.json else None
+        study = request.json['study'] if 'study' in request.json else None
+        user = request.json['user'] if 'user' in request.json else None
+        where = request.json['where'] if 'where' in request.json else None
+        other_information = request.json['other_information'] if 'other_information' in request.json else None
+
+        if deployment is None or study is None or user is None:
+            return json_util.dumps({
+                "status_code": 400,
+                "message": "Please make sure deployment, study, user are provided."
+            }), 400    
+        else:        
+            return json_util.dumps({
+                "status_code": 200,
+                "message": "This is your treatment.", 
+                "treatment": assign_treatment(deployment, study, user, where, other_information)
+            }), 200
+
+
+    except Exception as e:
+        print(traceback.format_exc())
+        return json_util.dumps({
+            "status_code": 500,
+            "message": "Server is down please try again later."
+        }), 500
+    
+
+
+@user_interaction_apis.route("/apis/give_reward", methods=["POST"])
+def give_reward():
+    # read request body.
+    try:
+        start_time = time.time()
+        deployment = request.json['deployment'] if 'deployment' in request.json else None
+        study = request.json['study'] if 'study' in request.json else None
+        user = request.json['user'] if 'user' in request.json else None
+        where = request.json['where'] if 'where' in request.json else None
+        other_information = request.json['other_information'] if 'other_information' in request.json else None
+        value = request.json['value'] if 'value' in request.json else None
+
+        if deployment is None or study is None or user is None or value is None:
+            return json_util.dumps({
+                "status_code": 400,
+                "message": "Please make sure deployment, study, user are provided."
+            }), 400    
+        else:        
+            result = get_reward(deployment, study, user, value, where, other_information)
+            if result != 200:
+                return json_util.dumps({
+                    "status_code": 400,
+                    "message": "No reward is saved."
+                }), 400
+            else:
+
+                return json_util.dumps({
+                    "status_code": 200,
+                    "message": "Reward is saved."
+                }), 200
+
+
+    except Exception as e:
+        print(e)
+        return json_util.dumps({
+            "status_code": 500,
+            "message": "Server is down please try again later."
+        }), 500
+
+
+
+def give_variable_value(deployment, study, variableName, user, value, where = None, other_information = None):
+
+    the_deployment = DeploymentModel.get_one({"name": deployment}, public = True)
+    the_study = StudyModel.get_one({"name": study, "deploymentId": the_deployment['_id'], "variables": {"$elemMatch": {"name": variableName}}})
+
+    if the_study is None:
+        return 400
+    current_time = datetime.datetime.now()
+    the_variable = {
+        "variableName": variableName,
+        "user": user,
+        "value": value,
+        "where": where,
+        "other_information": other_information,
+        "timestamp": current_time
+    }
+    VariableValueModel.insert_variable_value(the_variable)
+    return 200
+
+@user_interaction_apis.route("/apis/give_variable", methods=["POST"])
+def give_variable():
+    # save a contextual varialble.
+    try:
+        deployment = request.json['deployment'] if 'deployment' in request.json else None
+        study = request.json['study'] if 'study' in request.json else None
+        variableName = request.json['variableName'] if 'variableName' in request.json else None
+        user = request.json['user'] if 'user' in request.json else None
+        value = request.json['value'] if 'value' in request.json else None
+        where = request.json['where'] if 'where' in request.json else None
+        other_information = request.json['other_information'] if 'other_information' in request.json else None
+
+        if deployment is None or study is None or variableName is None or user is None or value is None:
+            return json_util.dumps({
+                "status_code": 400,
+                "message": "Please make sure variableName, user, value are provided."
+            }), 400
+        else:
+            doc = VariableModel.get_one({"name": variableName}, public = True)
+            if doc is None:
+                return json_util.dumps({
+                    "status_code": 400,
+                    "message": "Variable is not found."
+                }), 400
+        
+
+            
+            give_variable_value(deployment, study, variableName, user, value, where, other_information)
+            return json_util.dumps({
+                "status_code": 200,
+                "message": "Variable is saved."
+            }), 200
+    except Exception as e:
+        print("Error in giving_variable:")
+        print(e)
+        return json_util.dumps({
+            "status_code": 500,
+            "message": "Server is down please try again later."
+        }), 500
+
+
