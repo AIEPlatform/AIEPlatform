7107eae7bf5452b2830fa1e53c4e31191ed26698
ChenPanXYZ <me@chenpan.xyz>
2023-06-28 14:14:12 -0400

    added support for text contextuals.

--- backend/Models/VariableValueModel.py
+++ backend/Models/VariableValueModel.py
@@ -1,5 +1,10 @@
 from credentials import *
 from Models.VariableModel import VariableModel
+import openai
+
+openai.api_key = OPEN_AI_KEY
+
+
 class VariableValueModel:
     # Static methods:
     # Get the latest value for each of the given variables for the given user.
@@ -44,9 +49,24 @@ class VariableValueModel:
         try:
             # Need to check if it is a text variable or not.
             theVariable = VariableModel.get_one({'name': variable['variableName']}, public = True, session = session)
-            print(theVariable)
+            if theVariable['type'] == 'text':
+                print(VariableValueModel.gpt_reader(theVariable, variable['value']))
+                return 200
             VariableValue.insert_one(variable, session = session)
             return 200
         except Exception as e:
             print(e)
             return 500
+        
+    @staticmethod
+    def gpt_reader(variable, value):
+        messages = []
+        messages.append({"role": "system", "content": "Please return a numeric without any extra information."})
+        messages.append({"role": "system", "content": "Please always return a numeric, do not ask follow questions. If you think a text is very bad or irrelevant, feel free to give the min value."})
+        messages.append({"role": "system", "content": variable['variableValuePrompt']})
+        messages.append({"role": "user", "content": "A text to measure: " + value})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        return reply
